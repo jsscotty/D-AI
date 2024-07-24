6ea782e4927cb2cd0a3fb47688b186d6a80c22cc
chrisblona <christina@blona.de>
2024-07-24 12:12:43 +0200

    language switcher component

---
+++ web/src/components/LanguageSwitcher.tsx
@@ -0,0 +1,51 @@
+// basic language switcher
+"use client";
+
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Button,
+  Icon,
+} from "@chakra-ui/react";
+import { Link, usePathname, useRouter } from "@/navigation";
+import { useState, useRef } from "react";
+import { useTranslations } from "next-intl";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import { GB, DE } from "country-flag-icons/react/3x2";
+
+export default function LanguageSwitcher() {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const trans = useTranslations("locale");
+  const locale = trans("locale");
+
+  function toggleLocale(new_locale: "en" | "de") {
+    router.replace(pathname, { locale: new_locale });
+  }
+
+  const menuButtonRef = useRef(null);
+
+  return (
+    <Menu>
+      <MenuButton
+        as={Button}
+        rightIcon={<ChevronDownIcon />}
+        ref={menuButtonRef}
+        size={"sm"}
+      >
+        {locale == "de" ? <Icon as={DE} /> : <Icon as={GB} />}
+      </MenuButton>
+      <MenuList>
+        <MenuItem icon={<Icon as={GB} />} onClick={() => toggleLocale("en")}>
+          English
+        </MenuItem>
+        <MenuItem icon={<Icon as={DE} />} onClick={() => toggleLocale("de")}>
+          Deutsch
+        </MenuItem>
+      </MenuList>
+    </Menu>
+  );
+}
